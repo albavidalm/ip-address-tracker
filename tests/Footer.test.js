769645b3102577186b14a6e9d5776ae40b10b85f
albavidalm <albavidalm@gmail.com>
2023-04-12 16:49:28 +0200

    Refactor Footer test

--- tests/Footer.test.js
+++ tests/Footer.test.js
@@ -3,15 +3,20 @@ import { render, screen } from "@testing-library/react";
 import React from "react";
 
 describe("Footer component", () => {
-  test("author has to be in the footer", () => {
+  let author;
+  let linkedin;
+
+  beforeEach(() => {
     render(<Footer />);
-    const author = screen.getByText(/Alba Vidal/i);
+    author = screen.getByText(/Alba Vidal/i);
+    linkedin = screen.getByTitle("Linkedin");
+  });
+
+  test("author has to be in the footer", () => {
     expect(author).toBeInTheDocument();
   });
 
   test("Linkedin is in the footer", () => {
-    render(<Footer />);
-    const linkedin = screen.getByTitle("Linkedin");
     expect(linkedin).toBeInTheDocument();
     expect(linkedin.href).toBe("https://www.linkedin.com/in/albavidalm/");
     expect(linkedin.target).toBe("_blank");
